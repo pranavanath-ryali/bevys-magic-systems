503e34b9698b187ff4807711b003fef0daa05c20
pranavanath-ryali <dev.nitrostation@gmail.com>
2026-04-30 18:27:27 +0530

    added direnv

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+	};
+
+	outputs = { self, nixpkgs }: let
+		pkgs = nixpkgs.legacyPackages."x86_64-linux";
+	in {
+		devShells."x86_64-linux".default = pkgs.mkShell {
+			buildInputs = with pkgs; [
+				cargo
+				rustc
+				rustfmt
+				clippy
+				rust-analyzer
+				lldb
+				rustup
+				gdb
+				perf
+			];
+
+			nativeBuildInputs = with pkgs; [];
+
+			env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+		};
+	};
+}
